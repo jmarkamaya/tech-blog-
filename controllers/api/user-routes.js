8b698b484bf22c33cae446a72fd7a8889595d1f3
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-09-19 13:26:11 -0700

    finish setting up auth/debugging, finally got it working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,55 +1,49 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+      res.json(newUser);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
-
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
     });
-
+    if (!user) {
+      res.status(400).json({ message: 'No user account found!' });
+      return;
+    }
+    const validPassword = user.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'No user account found!' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.userId = user.id;
+      req.session.username = user.username;
+      req.session.loggedIn = true;
+      res.json({ user, message: 'You are now logged in!' });
+    });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({ message: 'No user account found!' });
   }
 });
-
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
@@ -57,5 +51,4 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
-
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,12 +1,12 @@
 const signupFormHandler = async function(event) {
     event.preventDefault();
-    const usernameEl = document.querySelector('#username-signup');
-    const passwordEl = document.querySelector('#password-signup');
+    const username = document.querySelector('#username-signup');
+    const password = document.querySelector('#password-signup');
     const response = await fetch('/api/user', {
       method: 'POST',
       body: JSON.stringify({
-        username: usernameEl.value,
-        password: passwordEl.value,
+        username: username.value,
+        password: password.value,
       }),
       headers: { 'Content-Type': 'application/json' },
     });
